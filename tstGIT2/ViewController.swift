4063e0d4ca403cf48ff331eb0fdedf641bc531b0
Mats Hammarqvist <Mats@Matss-MacBook-Pro.local>
2017-02-03 21:10:21 +0100

    Initial Commit

---
+++ tstGIT2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tstGIT2
+//
+//  Created by Mats Hammarqvist on 2017-02-03.
+//  Copyright © 2017 Mats Hammarqvist. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
