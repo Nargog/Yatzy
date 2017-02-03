d049cdf74582b808dfbbb8765226b9aed707b7de
Mats Hammarqvist <Mats@Matss-MacBook-Pro.local>
2017-02-03 21:35:07 +0100

    engelsk variant

--- tstGIT2/ViewController.swift
+++ tstGIT2/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func btnTest(_ sender: Any) {
-        lblText.text = "Det fungerar knappt"
+        lblText.text = "It works"
     }
 
     @IBOutlet weak var lblText: UILabel!
