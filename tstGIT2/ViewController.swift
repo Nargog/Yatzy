e2b1bd035258c416d733d9d388ffaa16036c2770
Mats Hammarqvist <Mats@Matss-MacBook-Pro.local>
2017-02-04 17:13:14 +0100

    ändring från MacBook

--- tstGIT2/ViewController.swift
+++ tstGIT2/ViewController.swift
@@ -21,7 +21,7 @@ class ViewController: UIViewController {
     }
 
     @IBAction func btnTest(_ sender: Any) {
-        lblText.text = "Det fungerar knappt"
+        lblText.text = "Det fungerar knappt - ändring från MacBook"
     }
 
     @IBOutlet weak var lblText: UILabel!
